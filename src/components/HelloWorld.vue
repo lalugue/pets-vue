ff1790a1f2f2ec4dd7e7d29f9e4da5b816e20318
lalugue <51893731+lalugue@users.noreply.github.com>
2021-03-10 23:23:55 +0800

    add landing page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<title>Pets - Home</title>
+
+  <body>
+    <div class="bs-component">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="home.html">Pets</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">                    
+            <li class="nav-item">
+              <a class="nav-link" href="dog.html">Dogs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cat.html">Cats</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Fish</a>
+            </li>                    
+          </ul>                  
+        </div>
+      </nav> 
+    </div>        
+
+    <div class="container">
+      <h1>
+        Pets
+      </h1>
+      <p>A small site for my favorite pets</p>
+
+      <a href="dog.html">
+        <i class="fas fa-dog pets-large-icon grow"></i>
+      </a>
+      <a href="cat.html">
+        <i class="fas fa-cat pets-large-icon grow"></i>
+      </a>
+      <a href="fish.html">
+        <i class="fas fa-fish pets-large-icon grow"></i>
+      </a>	  
+      </div>  
+      </body>
+
+      <footer class="footer">      
+        Check out my other stuff at <a href="https://github.com/lalugue">Github</a> - Theme from <a href="https://bootswatch.com/">Bootswatch</a>      
+      </footer>	  
+   
 </template>
 
 <script>
