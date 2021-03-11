00ff1d03064685c022f2a6a2e4734aa443ccfad0
lalugue <51893731+lalugue@users.noreply.github.com>
2021-03-11 23:12:03 +0800

    define pet component

---
+++ src/components/Pet.vue
@@ -0,0 +1,14 @@
+<template>
+<p>Pet is {{pet_name}}</p>
+</template>
+
+<script>
+export default {
+
+  data: function(){
+    return{
+      pet_name: this.$route.params.pet_name
+    }
+  } 
+}
+</script>
