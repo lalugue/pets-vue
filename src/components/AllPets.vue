65a5c34fd19451d0cb364dc10e0b4a2f797bbd83
lalugue <51893731+lalugue@users.noreply.github.com>
2021-03-12 01:23:04 +0800

    define AllPets component

---
+++ src/components/AllPets.vue
@@ -0,0 +1,13 @@
+<template>
+<router-link :to="{ name: 'pet', params: { pet_name: 'dog' }}">        
+         <i class="orange fas fa-dog pets-large-icon grow"></i>        
+      </router-link>
+      
+      <a href="cat.html">
+        <i class="fas fa-cat pets-large-icon grow"></i>
+      </a>
+      <a href="fish.html">
+        <i class="fas fa-fish pets-large-icon grow"></i>
+      </a>	  
+      
+</template>
