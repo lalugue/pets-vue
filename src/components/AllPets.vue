ea62963e899af17b06407b63ab010ff616c67793
lalugue <51893731+lalugue@users.noreply.github.com>
2021-03-12 23:06:35 +0800

    add cat and fish

--- src/components/AllPets.vue
+++ src/components/AllPets.vue
@@ -3,11 +3,11 @@
          <i class="orange fas fa-dog pets-large-icon grow"></i>        
       </router-link>
       
-      <a href="cat.html">
+      <router-link :to="{ name: 'pet', params: { pet_name: 'cat' }}"> 
         <i class="fas fa-cat pets-large-icon grow"></i>
-      </a>
-      <a href="fish.html">
+      </router-link>
+      <router-link :to="{ name: 'pet', params: { pet_name: 'fish' }}"> 
         <i class="fas fa-fish pets-large-icon grow"></i>
-      </a>	  
+      </router-link>	  
       
 </template>
